4eac677126c326b969d779399b8d3e719e2772d2
StevenKitavi <steven.kitavi@gmail.com>
2025-07-19 23:06:01 +0300

    Designed models

--- src/courses/models.py
+++ src/courses/models.py
@@ -6,6 +6,9 @@ class AccessRequirement(models.TextChoices):
     ANYONE = "any", "Anyone"
     EMAIL_REQUIRED = "email_required", "Email required"
 
+def handle_upload(instance, filename):
+
+    return f"{filename}"
 
 class PublishStatus(models.TextChoices):
     PUBLISHED = "pub", "Published"
@@ -15,15 +18,15 @@ class PublishStatus(models.TextChoices):
 class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    #image
+    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
     access = models.CharField(
-        max_length = 10,
+        max_length = 14,
         choices = AccessRequirement.choices,
-        default= AccessRequirement.ANYONE
+        default= AccessRequirement.EMAIL_REQUIRED
         )
 
     status = models.CharField(
-        max_length=10,
+        max_length= 14,
         choices=PublishStatus.choices,
         default = PublishStatus.DRAFT
     )
